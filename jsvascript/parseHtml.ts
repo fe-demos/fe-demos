743a5c0efa258fc559641725971beed41d025c1e
YuTengjing <ytj2713151713@gmail.com>
2024-02-01 12:07:54 +0800

    optimize parseHtml

--- jsvascript/parseHtml.ts
+++ jsvascript/parseHtml.ts
@@ -1,4 +1,25 @@
-const input = '<div><h1> Title </h1><p>Some description. </p></div>';
+function findCloseTagIndex(html: string, openTag: string, closeTag: string) {
+    const tagRegexp = new RegExp(`${openTag}|${closeTag}`, 'g');
+    let match = tagRegexp.exec(html);
+    let openTagCount = 0;
+    let closeTagCount = 0;
+    while (match) {
+        if (match[0].includes('/')) {
+            closeTagCount++;
+        } else {
+            openTagCount++;
+        }
+
+        if (closeTagCount === openTagCount) {
+            return match.index;
+        }
+        match = tagRegexp.exec(html);
+    }
+    return -1;
+}
+
+const input = '<div><div>6</div><h1> Title </h1><p>Some description. </p></div>';
+
 function parseHtml(html: string): any {
     const elements = [];
     const openTagRegexp = /^<(\w+)>/;
@@ -9,8 +30,14 @@ function parseHtml(html: string): any {
             const openTag = match[0];
             const tagName = match[1];
             const closeTag = `</${tagName}>`;
-            const closeTagIndex = html.indexOf(closeTag);
+            // 需要考虑子节点标签和父标签相同的情况，所以不能 closeTagIndex = html.indexOf(closeTag)
+            const closeTagIndex = findCloseTagIndex(html, openTag, closeTag);
             const childrenHtml = html.slice(openTag.length, closeTagIndex);
+            // console.log({
+            //     openTag,
+            //     closeTag,
+            //     childrenHtml,
+            // });
             html = html.slice(closeTagIndex + closeTag.length);
             const children = parseHtml(childrenHtml);
             const element = {
@@ -37,17 +64,27 @@ function parseHtml(html: string): any {
 
     return null;
 }
-console.log(parseHtml(input));
+console.log(JSON.stringify(parseHtml(input), null, 4));
 // {
-//     tagName: 'div',
-//     children: [
+//     "tagName": "div",
+//     "children": [
 //         {
-//             tagName: 'h1',
-//             children: ['Title']
+//             "tagName": "div",
+//             "children": [
+//                 "6"
+//             ]
 //         },
 //         {
-//             tagName: 'p',
-//             children: ['Some description.']
+//             "tagName": "h1",
+//             "children": [
+//                 "Title"
+//             ]
+//         },
+//         {
+//             "tagName": "p",
+//             "children": [
+//                 "Some description."
+//             ]
 //         }
 //     ]
 // }
